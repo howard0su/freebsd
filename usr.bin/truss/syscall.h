969757f280b38b97e7c7055057f7c2b835e8de5f
John Baldwin <jhb@FreeBSD.org>
2014-10-03 13:09:54 -0400

    Parse _umtx_op() arguments in kdump and truss.

--- usr.bin/truss/syscall.h
+++ usr.bin/truss/syscall.h
@@ -35,13 +35,13 @@
  * $FreeBSD$
  */
 
-enum Argtype { None = 1, Hex, Octal, Int, Name, Ptr, Stat, Ioctl, Quad,
+enum Argtype { None = 1, Hex, Octal, Int, LongHex, Name, Ptr, Stat, Ioctl, Quad,
 	Signal, Sockaddr, StringArray, Timespec, Timeval, Itimerval, Pollfd,
 	Fd_set, Sigaction, Fcntl, Mprot, Mmapflags, Whence, Readlinkres,
 	Umtx, Sigset, Sigprocmask, Kevent, Sockdomain, Socktype, Open,
 	Fcntlflag, Rusage, BinString, Shutdown, Resource, Rlimit, Timeval2,
 	Pathconf, Rforkflags, ExitStatus, Waitoptions, Idtype, Procctl,
-	LinuxSockArgs };
+	LinuxSockArgs, Umtxop };
 
 #define	ARG_MASK	0xff
 #define	OUT	0x100
