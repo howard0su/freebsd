f1de63da8e954631e7a24a183f82760b16b34598
John Baldwin <jhb@FreeBSD.org>
2014-10-28 12:03:55 -0400

    Update license for vmware.h per jkim@.

--- sys/x86/include/vmware.h
+++ sys/x86/include/vmware.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 1998-2003 Poul-Henning Kamp
+ * Copyright (c) 2011-2014 Jung-uk Kim <jkim@FreeBSD.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
