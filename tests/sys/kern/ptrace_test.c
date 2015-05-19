c59aba3945aff76ba362131f4cd7cd8f770fc264
John Baldwin <jhb@FreeBSD.org>
2015-05-19 17:36:15 -0400

    Add the same double-wait test for PT_ATTACH().

--- tests/sys/kern/ptrace_test.c
+++ tests/sys/kern/ptrace_test.c
@@ -38,7 +38,7 @@ __FBSDID("$FreeBSD$");
 
 /*
  * Verify that a parent debugger process "sees" the exit of a debugged
- * process exactly once.
+ * process exactly once when attached via PT_TRACE_ME.
  */
 ATF_TC_WITHOUT_HEAD(ptrace__parent_wait_after_trace_me);
 ATF_TC_BODY(ptrace__parent_wait_after_trace_me, tc)
@@ -80,10 +80,64 @@ ATF_TC_BODY(ptrace__parent_wait_after_trace_me, tc)
 	ATF_REQUIRE(errno == ECHILD);
 }
 
+/*
+ * Verify that a parent debugger process "sees" the exit of a debugged
+ * process exactly once when attached via PT_ATTACH.
+ */
+ATF_TC_WITHOUT_HEAD(ptrace__parent_wait_after_attach);
+ATF_TC_BODY(ptrace__parent_wait_after_attach, tc)
+{
+	pid_t child, wpid;
+	int cpipe[2], status;
+	char c;
+
+	ATF_REQUIRE(pipe(cpipe) == 0);
+	ATF_REQUIRE((child = fork()) != -1);
+	if (child == 0) {
+		/* Child process. */
+		close(cpipe[0]);
+
+		/* Wait for the parent to attach. */
+		ATF_REQUIRE(read(cpipe[1], &c, sizeof(c)) == 0);
+
+		exit(1);
+	}
+	close(cpipe[1]);
+
+	/* Parent process. */
+
+	/* Attach to the child process. */
+	ATF_REQUIRE(ptrace(PT_ATTACH, child, NULL, 0) == 0);
+
+	/* The first wait() should report the SIGSTOP from PT_ATTACH. */
+	wpid = waitpid(child, &status, 0);
+	ATF_REQUIRE(wpid == child);
+	ATF_REQUIRE(WIFSTOPPED(status));
+	ATF_REQUIRE(WSTOPSIG(status) == SIGSTOP);
+
+	/* Continue the child ignoring the SIGSTOP. */
+	ATF_REQUIRE(ptrace(PT_CONTINUE, child, (caddr_t)1, 0) != -1);
+
+	/* Signal the child to exit. */
+	close(cpipe[0]);
+
+	/* The second wait() should report the exit status. */
+	wpid = waitpid(child, &status, 0);
+	ATF_REQUIRE(wpid == child);
+	ATF_REQUIRE(WIFEXITED(status));
+	ATF_REQUIRE(WEXITSTATUS(status) == 1);
+
+	/* The child should no longer exist. */
+	wpid = waitpid(child, &status, 0);
+	ATF_REQUIRE(wpid == -1);
+	ATF_REQUIRE(errno == ECHILD);
+}
+
 ATF_TP_ADD_TCS(tp)
 {
 
 	ATF_TP_ADD_TC(tp, ptrace__parent_wait_after_trace_me);
+	ATF_TP_ADD_TC(tp, ptrace__parent_wait_after_attach);
 
 	return (atf_no_error());
 }
