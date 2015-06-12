4914b58998fb5de1893e3e9e9ca3d80d6cbad8b3
John Baldwin <jhb@FreeBSD.org>
2015-06-12 09:00:32 -0400

    Don't panic if the MEM64 range is not present as qemu does not include it.

--- sys/sparc64/pci/psycho.c
+++ sys/sparc64/pci/psycho.c
@@ -294,7 +294,7 @@ psycho_attach(device_t dev)
 	phandle_t node;
 	uint32_t dvmabase, prop, prop_array[2];
 	u_int rerun, ver;
-	int i, j;
+	int i, j, nrange;
 
 	node = ofw_bus_get_node(dev);
 	sc = device_get_softc(dev);
@@ -446,26 +446,34 @@ psycho_attach(device_t dev)
 	    rman_manage_region(&sc->sc_pci_mem_rman, 0, PSYCHO_MEM_SIZE) != 0)
 		panic("%s: failed to set up memory rman", __func__);
 
-	i = OF_getprop_alloc(node, "ranges", sizeof(*range), (void **)&range);
-	/*
-	 * Make sure that the expected ranges are present.  The
-	 * OFW_PCI_CS_MEM64 one is not currently used though.
-	 */
-	if (i != PSYCHO_NRANGE)
-		panic("%s: unsupported number of ranges", __func__);
+	nrange = OF_getprop_alloc(node, "ranges", sizeof(*range),
+	    (void **)&range);
+
 	/*
 	 * Find the addresses of the various bus spaces.
 	 * There should not be multiple ones of one kind.
 	 * The physical start addresses of the ranges are the configuration,
 	 * memory and I/O handles.
 	 */
-	for (i = 0; i < PSYCHO_NRANGE; i++) {
+	for (i = 0; i < nrange; i++) {
 		j = OFW_PCI_RANGE_CS(&range[i]);
 		if (sc->sc_pci_bh[j] != 0)
 			panic("%s: duplicate range for space %d",
 			    __func__, j);
 		sc->sc_pci_bh[j] = OFW_PCI_RANGE_PHYS(&range[i]);
 	}
+
+	/*
+	 * Make sure that the expected ranges are present.  The
+	 * OFW_PCI_CS_MEM64 one is not currently used.
+	 */
+	if (sc->sc_pci_bh[OFW_PCI_CS_CONFIG] == 0)
+		panic("%s: missing CONFIG range", __func__);
+	if (sc->sc_pci_bh[OFW_PCI_CS_IO] == 0)
+		panic("%s: missing IO range", __func__);
+	if (sc->sc_pci_bh[OFW_PCI_CS_MEM32] == 0)
+		panic("%s: missing MEM32 range", __func__);
+
 	free(range, M_OFWPROP);
 
 	/* Register the softc, this is needed for paired Psychos. */
