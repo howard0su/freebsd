7c7f075bd3d306abdbe40a5f2dcc27bb736bce90
John Baldwin <jhb@FreeBSD.org>
2015-08-30 00:08:47 -0700

    Cleanup includes.

--- usr.bin/truss/main.c
+++ usr.bin/truss/main.c
@@ -38,20 +38,10 @@ __FBSDID("$FreeBSD$");
  * do a lot of the work :).
  */
 
-#include <sys/param.h>
-#include <sys/types.h>
-#include <sys/time.h>
-#include <sys/resource.h>
-#include <sys/sysctl.h>
-#include <sys/wait.h>
-
 #include <err.h>
-#include <errno.h>
-#include <fcntl.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
 #include <unistd.h>
 
