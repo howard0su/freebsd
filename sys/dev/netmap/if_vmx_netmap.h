b7e01384a44c2785530bdf78169daea22e4d2e11
John Baldwin <jhb@FreeBSD.org>
2015-10-12 15:39:01 -0700

    Newlines.

--- sys/dev/netmap/if_vmx_netmap.h
+++ sys/dev/netmap/if_vmx_netmap.h
@@ -306,7 +306,7 @@ vmxnet3_netmap_rxsync(struct netmap_kring *kring, int flags)
 		kring->nr_hwtail = nm_i;
 		kring->nr_kflags &= ~NKR_PENDINTR;
 	}
-        printf("[B] h %d c %d hwcur %d hwtail %d",
+        printf("[B] h %d c %d hwcur %d hwtail %d\n",
 		ring->head, ring->cur, kring->nr_hwcur,
 			      kring->nr_hwtail);
 
@@ -354,7 +354,7 @@ vmxnet3_netmap_rxsync(struct netmap_kring *kring, int flags)
 	/* tell userspace that there might be new packets. */
 	nm_rxsync_finalize(kring);
 
-        printf("[C] h %d c %d t %d hwcur %d hwtail %d",
+        printf("[C] h %d c %d t %d hwcur %d hwtail %d\n",
 		ring->head, ring->cur, ring->tail,
 		kring->nr_hwcur, kring->nr_hwtail);
 
