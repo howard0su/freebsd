050269e615b6f54ed2ad8d89c334892021359392
John Baldwin <jhb@FreeBSD.org>
2015-10-14 14:00:36 -0700

    Account for page holds and wirings to check for leaks.

--- sys/dev/cxgbe/tom/t4_ddp.c
+++ sys/dev/cxgbe/tom/t4_ddp.c
@@ -95,6 +95,12 @@ SYSCTL_ULONG(_hw_cxgbe_ddp, OID_AUTO, aio_copied, CTLFLAG_RW, &ddp_aio_copied,
 static unsigned long ddp_aio_mixed = 0;
 SYSCTL_ULONG(_hw_cxgbe_ddp, OID_AUTO, aio_mixed, CTLFLAG_RW, &ddp_aio_mixed,
     0, "AIO requests completed via copies and DDP");
+static unsigned long ddp_held_pages = 0;
+SYSCTL_ULONG(_hw_cxgbe_ddp, OID_AUTO, held_pages, CTLFLAG_RW, &ddp_held_pages,
+    0, "Pages held for DDP");
+static unsigned long ddp_wired_pages = 0;
+SYSCTL_ULONG(_hw_cxgbe_ddp, OID_AUTO, held_pages, CTLFLAG_RW, &ddp_held_pages,
+    0, "Pages wired for DDP");
 
 #if 0
 static struct mbuf *get_ddp_mbuf(int len);
@@ -1124,11 +1130,13 @@ select_ddp_buffer(struct adapter *sc, struct toepcb *toep, vm_page_t *pages,
 	db = alloc_ddp_buffer(td, pages, npages, db_off, db_len);
 	if (db == NULL) {
 		vm_page_unhold_pages(pages, npages);
+		atomic_sub_long(&ddp_held_pages, npages);
 		free(pages, M_CXGBE);
 		return (-1);
 	}
 	if (write_page_pods(sc, toep, db) != 0) {
 		vm_page_unhold_pages(pages, npages);
+		atomic_sub_long(&ddp_held_pages, npages);
 		free_ddp_buffer(td, db);
 		return (-1);
 	}
@@ -1161,7 +1169,9 @@ wire_ddp_buffer(struct ddp_buffer *db)
 		p = db->pages[i];
 		vm_page_lock(p);
 		vm_page_wire(p);
+		atomic_add_long(&ddp_wired_pages, 1);
 		vm_page_unhold(p);
+		atomic_sub_long(&ddp_held_pages, 1);
 		vm_page_unlock(p);
 	}
 }
@@ -1176,6 +1186,7 @@ unwire_ddp_buffer(struct ddp_buffer *db)
 		p = db->pages[i];
 		vm_page_lock(p);
 		vm_page_unwire(p, PQ_INACTIVE);
+		atomic_sub_long(&ddp_wired_pages, 1);
 		vm_page_unlock(p);
 	}
 }
@@ -1625,6 +1636,7 @@ hold_aio(struct aiocblist *aiocbe, vm_page_t **ppages, int *pnpages,
 
 	*ppages = pp;
 	*pnpages = n;
+	atomic_add_long(&ddp_held_pages, n);
 	return (0);
 }
 
@@ -1743,6 +1755,7 @@ restart:
 		error = so->so_error;
 		so->so_error = 0;
 		vm_page_unhold_pages(pages, npages);
+		atomic_sub_long(&ddp_held_pages, npages);
 		aio_complete(cbe, -1, error);
 		toep->ddp_queueing = NULL;
 		goto restart;
@@ -1750,6 +1763,7 @@ restart:
 
 	if (sb->sb_state & SBS_CANTRCVMORE && sbavail(sb) == 0) {
 		vm_page_unhold_pages(pages, npages);
+		atomic_sub_long(&ddp_held_pages, npages);
 		if (toep->ddp_active_count != 0) {
 			TAILQ_INSERT_HEAD(&toep->ddp_aiojobq, cbe, list);
 			toep->ddp_waiting_count++;
@@ -1808,6 +1822,7 @@ restart:
 			 */
 			SOCKBUF_UNLOCK(sb);
 			vm_page_unhold_pages(pages, npages);
+			atomic_sub_long(&ddp_held_pages, npages);
 
 			/* Notify protocol that we drained some data. */
 			if (so->so_proto->pr_flags & PR_WANTRCVD) {
@@ -1829,6 +1844,7 @@ restart:
 		 */
 		if ((toep->ddp_flags & (DDP_ON | DDP_SC_REQ)) != DDP_ON) {
 			vm_page_unhold_pages(pages, npages);
+			atomic_sub_long(&ddp_held_pages, npages);
 			TAILQ_INSERT_HEAD(&toep->ddp_aiojobq, cbe, list);
 			toep->ddp_waiting_count++;
 			toep->ddp_queueing = NULL;
@@ -1906,7 +1922,11 @@ restart:
 		 * start of the task to avoid having to handle this
 		 * edge case.
 		 */
+		TAILQ_INSERT_HEAD(&toep->ddp_aiojobq, cbe, list);
+		toep->ddp_waiting_count++;
+		toep->ddp_queueing = NULL;
 		vm_page_unhold_pages(db->pages, db->npages);
+		atomic_sub_long(&ddp_held_pages, db->npages);
 		SOCKBUF_UNLOCK(sb);
 		printf("%s: mk_update_tcb_for_ddp failed\n", __func__);
 		return;
