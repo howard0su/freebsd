9cb324521696bd3d3f03c5d58c7b2e96bb4ebffb
John Baldwin <jhb@FreeBSD.org>
2015-12-21 11:55:51 -0800

    Rework the TDB_EXIT reporting to avoid sleeping with the tidhash lock held.
    To avoid losing the race of the last thread exiting, add a count of "exiting"
    threads.  A thread marks itself as exiting while it is blocked in ptracestop()
    as well as while the PROC_LOCK is dropped to remove the thread from the
    tidhash.  Each thread compares p_exitingthreads against p_numthreads to
    determine if it is the last thread.  If it is, it waits for any "exiting"
    threads to finish exiting before it returns.
    Instead, add a count of "exiting" threads

--- sys/sys/proc.h
+++ sys/sys/proc.h
@@ -579,6 +579,7 @@ struct proc {
 	struct itimers	*p_itimers;	/* (c) POSIX interval timers. */
 	struct procdesc	*p_procdesc;	/* (e) Process descriptor, if any. */
 	u_int		p_treeflag;	/* (e) P_TREE flags */
+	int		p_exitingthreads; /* (c) Count of exiting threads. */
 /* End area that is zeroed on creation. */
 #define	p_endzero	p_magic
 
