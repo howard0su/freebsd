73fdd90fd5482082fb3dccaa032539746d972c3c
Howard Su <howard0su@gmail.com>
2016-04-07 20:55:27 +0800

    hyperv: get hyperv code into a self-contain .ko

--- sys/dev/hyperv/vmbus/hv_vmbus_drv_freebsd.c
+++ sys/dev/hyperv/vmbus/hv_vmbus_drv_freebsd.c
@@ -383,9 +383,7 @@ vmbus_probe(device_t dev) {
 	return (BUS_PROBE_DEFAULT);
 }
 
-#ifdef HYPERV
 extern inthand_t IDTVEC(hv_vmbus_callback);
-#endif
 
 /**
  * @brief hv_vmbus_synic_init
@@ -540,14 +538,10 @@ vmbus_bus_init(void)
 
 	vmbus_inited = 1;
 
-#ifdef HYPERV
 	/*
 	 * Find a free IDT slot for vmbus callback.
 	 */
 	hv_vmbus_g_context.hv_cb_vector = lapic_ipi_alloc(IDTVEC(hv_vmbus_callback));
-#else
-	hv_vmbus_g_context.hv_cb_vector = -1;
-#endif
 	if (hv_vmbus_g_context.hv_cb_vector < 0) {
 		if(bootverbose)
 			printf("Error VMBUS: Cannot find free IDT slot for "
